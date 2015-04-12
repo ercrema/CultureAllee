6967ba6f02779d29d3ffce6970e7c412db1e3bb3
Enrico Crema <enrico.crema@gmail.com>
2015-04-12 13:40:29 +0200

    Check for stability/instability in FigureX

--- SAAanlysis/saaPlot.R
+++ SAAanlysis/saaPlot.R
@@ -86,3 +86,35 @@ abline(v=c(Am,Km),lty=2,col="white")
 axis(side=3,at=c(Am,Km),labels=c(expression(A[m]),expression(K[m])),padj=0.8)
 axis(side=4,at=c(An,Kn),labels=c(expression(A[n]),expression(K[n])),hadj=0.5,las=2)
 dev.print(device=pdf,"./SAAanlysis/figX.pdf")
+
+#timeseries check to see whether greens are actually coexistence and orange are actually unstable
+Em=ExpStableNode(Kn,Km,cKn,cKm)$m
+En=ExpStableNode(Kn,Km,cKn,cKm)$n
+
+#deal with floating point arithmethic problem#
+
+
+figureX$m.final[which(as.logical(sapply(figureX$m.final,all.equal,target=Km,USE.NAMES=F,simplify=TRUE)))]=Km
+figureX$n.final[which(as.logical(sapply(figureX$n.final,all.equal,target=Kn,USE.NAMES=F,simplify=TRUE)))]=Kn
+figureX$m.final[which(as.logical(sapply(figureX$m.final,all.equal,target=Em,USE.NAMES=F,simplify=TRUE)))]=Em
+figureX$n.final[which(as.logical(sapply(figureX$n.final,all.equal,target=En,USE.NAMES=F,simplify=TRUE)))]=En
+figureX$m.final[which(as.logical(sapply(figureX$m.final,all.equal,target=0,USE.NAMES=F,simplify=TRUE)))]=0
+figureX$n.final[which(as.logical(sapply(figureX$n.final,all.equal,target=0,USE.NAMES=F,simplify=TRUE)))]=0
+figureX$col=1
+figureX$col[which(figureX$m.final==Em&figureX$n.final==En)]=2
+figureX$col[which(figureX$m.final==Km&figureX$n.final==0)]=3   
+figureX$col[which(figureX$m.final==0&figureX$n.final==Kn)]=4      
+figureX$col[which(figureX$m.final==0&figureX$n.final==0)]=5
+
+
+
+possibleCoexistence=subset(figureX,col==2)
+possibleUnstable=subset(figureX,col==1)
+
+x=sample(size=1,nrow(possibleCoexistence))
+basePlot(ini.m=possibleCoexistence[x,1],ini.n=possibleCoexistence[x,2],
+    Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z,timeSteps=50000)
+
+x=sample(size=1,nrow(possibleUnstable))
+basePlot(ini.m=possibleUnstable[x,1],ini.n=possibleUnstable[x,2],
+    Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z,timeSteps=50000)
