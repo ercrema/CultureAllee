ecb1fed629ac50ada43640a0e6e73b4be205f836
Xavier Rubio <xrubio@gmail.com>
2015-05-22 11:46:55 -0700

    options for plot

--- manuscript/figures/figure6.R
+++ manuscript/figures/figure6.R
@@ -4,7 +4,7 @@ source("../../src/utility.R")
 
 
 library(ggplot2)
-
+library(gridExtra)
 
 getRawValues <- function(run)
 {
@@ -27,8 +27,8 @@ getRawValues <- function(run)
 
     resultdf <- data.frame(eq='coexistence', value=nrow(coexistence))
     resultdf <- rbind(resultdf, data.frame(eq='extinction', value=nrow(extinction)))
-    resultdf <- rbind(resultdf, data.frame(eq='mExtinct', value=nrow(mExtinct)))
-    resultdf <- rbind(resultdf, data.frame(eq='nExtinct', value=nrow(nExtinct)))
+    resultdf <- rbind(resultdf, data.frame(eq='a Extinct', value=nrow(mExtinct)))
+    resultdf <- rbind(resultdf, data.frame(eq='b Extinct', value=nrow(nExtinct)))
     resultdf <- rbind(resultdf, data.frame(eq='unstable', value=(nrow(result)-total)))
     
     resultdf$lambda = lambda
@@ -58,26 +58,39 @@ for(run in 0:109)
     raw <- rbind(raw, getRawValues(run))
 }
 
-#percentages <- getPercentages(raw)
+percentages <- getPercentages(raw)
 
-#pdf('figure6a.pdf')
-#ggplot(percentages, aes(x=lambda, y=value, group=z, col=factor(z))) + geom_line() + facet_wrap(~eq)
-#dev.off()
-#pdf('figure6b.pdf')    
-#ggplot(percentages, aes(x=lambda, y=value, fill=eq)) + geom_bar(stat='identity') + facet_wrap(~z, nrow=3)
-#dev.off()
+# coexistence: #006400
+# unstable: #D3D3D3
+# nExtinct: #4169E1
+# mExtinct: #CD5C5C
+
+pdf('figure6a.pdf', width=12, height=7)
+ggplot(subset(percentages, z<0.6), aes(x=lambda, y=value, fill=eq)) + geom_area(stat='identity') + scale_fill_manual(name='Equilibria', values=c('#006400','gold2','#4169E1','#CD5C5C','#D3D3D3'))+ facet_wrap(~z, nrow=2) + scale_x_reverse()
+dev.off()
+
+
+pdf('figure6b.pdf', width=15, height=7)
+ggplot(subset(percentages, z<0.6), aes(x=z, y=value, fill=eq)) + geom_area(stat='identity') + scale_fill_manual(name='Equilibria', values=c('#006400','gold2','#4169E1','#CD5C5C','#D3D3D3'))+ facet_wrap(~lambda, nrow=2)
+dev.off()
+
+pdf('figure6c.pdf', width=18, height=7)    
+g1 <- ggplot(subset(percentages, z==0), aes(x=lambda, y=value, fill=eq)) + geom_area(stat='identity') + scale_fill_manual(name='Equilibria', values=c('#006400','gold2','#4169E1','#CD5C5C','#D3D3D3')) + scale_x_reverse()+ theme(legend.position="bottom")
+g2 <- ggplot(subset(percentages, z>0 & z<0.7), aes(x=lambda, y=value, fill=eq)) + geom_area(stat='identity') + scale_fill_manual(name='Equilibria', values=c('#006400','gold2','#4169E1','#CD5C5C','#D3D3D3'))+ facet_wrap(~z, nrow=2) + scale_x_reverse()  + theme(legend.position="none")
+grid.arrange(arrangeGrob(g1,g2, widths=c(0.33,0.66),nrow=1))
+dev.off()
 
 # relative
-z0 <- subset(raw, z==0)
-relatives <- data.frame(raw)
-relatives$rel <- 0
+#z0 <- subset(raw, z==0)
+#relatives <- data.frame(raw)
+#relatives$rel <- 0
 
-for(value in 1:nrow(raw))
-{
-    relatives[value,]$rel <- getRelativeValue(raw[value,], z0)
-}
+#for(value in 1:nrow(raw))
+#{
+#    relatives[value,]$rel <- getRelativeValue(raw[value,], z0)
+#}
 
-pdf('relatives2.pdf')
-ggplot(subset(relatives, eq!='unstable' & z<0.6 & z>0), aes(x=lambda, y=rel, group=factor(z), col=factor(z))) + geom_line() + scale_x_reverse() + geom_hline(yintercept=1, linetype="dashed") + facet_wrap(~eq)
-dev.off()
+#pdf('relatives2.pdf')
+#ggplot(subset(relatives, eq!='unstable' & z<0.6 & z>0), aes(x=lambda, y=rel, group=factor(z), col=factor(z))) + geom_line() + scale_x_reverse() + geom_hline(yintercept=1, linetype="dashed") + facet_wrap(~eq)
+#dev.off()
 
