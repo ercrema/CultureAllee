a66a68dbaca4d4390b5f461ccf080d61526a1c41
Xavier Rubio <xrubio@gmail.com>
2015-06-01 16:59:45 -0700

    unstable figure

--- manuscript/figures/figureUnstable.R
+++ manuscript/figures/figureUnstable.R
@@ -5,11 +5,8 @@ source("../../src/alleeIGC.R")
 source("../../src/basin.R")
 source("../../src/utility.R")
 
-
-# load lambda=1, z=0.5
-#load('../../experimentMN3/02_relative_lambda/result_065.Rdata')
-# or load lambda=1, z=0.4
-load('../../experimentMN3/02_relative_lambda/result_054.Rdata')
+# lambda=0.9, z=0.4
+load('../../experimentMN3/05_unstability/result_003.Rdata')
 
 pdf('figureUnstable1.pdf', width=8, height=8)
 
@@ -18,16 +15,16 @@ abline(h=c(An,Kn),lty=2,col="white")
 abline(v=c(Am,Km),lty=2,col="white")
 axis(side=3,at=c(Am,Km),labels=c(expression(A[a]),expression(K[a])),padj=0.8)
 axis(side=4,at=c(An,Kn),labels=c(expression(A[b]),expression(K[b])),hadj=0.5,las=2)
-points(x=c(0.54,0.56), y=c(0.8, 0.8), pch=20)
+points(x=c(0.4,0.39), y=c(1.0, 0.99), pch=20)
 
 # text with buffer    
-text(0.52,0.8,labels="j",col='black',font=2, cex=1)
-text(0.58,0.8,labels="k",col='black',font=2, cex=1)
+text(0.41, 1.01,labels="k",col='black',font=2, cex=1)
+text(0.38,0.98,labels="j",col='black',font=2, cex=1)
 
 dev.off()
 
-stable <- allee(ini.m=0.55,ini.n=0.8,Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z)
-unstable <- allee(ini.m=0.56,ini.n=0.8,Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z)
+stable <- allee(ini.m=0.39,ini.n=0.99,Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z)
+unstable <- allee(ini.m=0.4,ini.n=1.0,Am=Am,Km=Km,An=An,Kn=Kn,cAn=cAn,cAm=cAm,cKn=cKn,cKm=cKm,r.m=r.m,r.n=r.n,z=z)
 
 stableA <- data.frame(step=1:length(stable$m), pop=stable$m, strategy='a', equilibrium='j')
 stableB <- data.frame(step=1:length(stable$n), pop=stable$n, strategy='b', equilibrium='j')
